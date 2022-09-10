34c615ac429e548c678cf62d04692556b38fc45e
mehidi47 <73783226+mehidi47@users.noreply.github.com>
2022-09-10 22:23:25 +0600

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const city = urlParams.get("city");
-console.log(city);
-
-let place = document.querySelector(".city");
-place.innerHTML = city;
